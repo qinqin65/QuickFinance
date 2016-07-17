26d3ec3eeb952216945afda4d6b0f911fe0cefce
qinqin65 <tom163com@126.com>
2016-07-17 21:35:05 +0800

    修改注册的处理逻辑

--- QuickFinance/quick/views.py
+++ QuickFinance/quick/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.template import RequestContext
 from django.views.decorators.http import require_POST
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as djangoLogin
 from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 from django.utils.translation import ugettext as _
-from django.core import serializers
+# from django.core import serializers
+from django.views.decorators.csrf import ensure_csrf_cookie
 from . import stateCode
 from .util import debug
-# from django.views.decorators.csrf import ensure_csrf_cookie
 
-# @ensure_csrf_cookie
+@ensure_csrf_cookie
 def home(request):
     #assert isinstance(request, HttpRequest)
     return render(
@@ -39,15 +40,15 @@ def login(request):
         userName = request.POST['userName']
         passWord = request.POST['password']
     except:
-        return JsonResponse({'state': stateCode.ERROR, 'info': _('normal error')})
+        return JsonResponse({'state': stateCode.ERROR, 'info': _('login failed')})
     else:
         user = authenticate(username=userName, password=passWord)
         if user is None:
             return JsonResponse({'state': stateCode.ERROR, 'info': _('user or password is invalid')})
         else:
             if user.is_active:
-                login(request, user)
-                return JsonResponse({'state': stateCode.SUCCESS, 'user': user})
+                djangoLogin(request, user)
+                return JsonResponse({'state': stateCode.SUCCESS, 'user': {'userName': user.username}})
             else:
                 return JsonResponse({'state': stateCode.ERROR, 'info': _('user is not allowed to login')})
 
@@ -59,9 +60,10 @@ def register(request):
         email = request.POST['email']
         user = User.objects.create_user(userName, email, passWord)
         user.save()
+    except IntegrityError as e:
+        return JsonResponse({'state': stateCode.ERROR, 'info': _('user already exist')})
     except Exception as e:
-        print(e)
-        return JsonResponse({'state': stateCode.ERROR, 'info': _('normal error')})
+        return JsonResponse({'state': stateCode.ERROR, 'info': _('register failed')})
     else:
-        return JsonResponse({'state': stateCode.SUCCESS, 'user': serializers.serialize('json', user)})
+        return JsonResponse({'state': stateCode.SUCCESS, 'user': {'userName': user.username}})
 
