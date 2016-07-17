e10b1c047537ce182dcf3608d68789e78c805744
qinqin65 <tom163com@126.com>
2016-07-17 21:31:49 +0800

    封装dojo的xhr方法,以自动添加csrf字段

---
+++ QuickFinance_frontEnd/app/xhr.ts
@@ -0,0 +1,9 @@
+import * as xhr from 'dojo/request/xhr';
+import Config from 'config';
+
+export let post = function(url: string, option: any) {
+    if(option.data && typeof option.data == 'object') {
+        option.data.csrfmiddlewaretoken = Config.csrf;
+    }
+    return xhr.post(url, option);
+}
