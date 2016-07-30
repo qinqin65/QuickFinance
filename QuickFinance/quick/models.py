bebcd5aea608de227651b819802305ed48f0a27b
qinqin65 <tom163com@126.com>
2016-07-31 00:14:42 +0800

    添加记账需要的数据库表

--- QuickFinance/quick/models.py
+++ QuickFinance/quick/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AccountBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    accountBookName = models.CharField(max_length=200)
+    remark = models.TextField()
+
+class Account(models.Model):
+    accountBook = models.ForeignKey(AccountBook, on_delete=models.CASCADE)
+    accountName = models.CharField(max_length=200)
+    total = models.IntegerField(default=0)
+    webUrl = models.URLField()
+    remark = models.TextField()
+
+class AccountType(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    remark = models.TextField()
+
+class Income(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    value = models.IntegerField()
+    remark = models.TextField()
+
+class Outcome(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    value = models.IntegerField()
+    remark = models.TextField()
