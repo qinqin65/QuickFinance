c7fd5ff53224b937ee9a54fd516d37eaa1288c0f
qinqin65 <tom163com@126.com>
2016-07-31 21:28:13 +0800

    账本数据从服务器端获取

--- QuickFinance_frontEnd/app/financeApp.tsx
+++ QuickFinance_frontEnd/app/financeApp.tsx
@@ -4,6 +4,7 @@ import * as topic from 'dojo/topic';
 import Config from 'config';
 import * as stateCode from 'stateCode';
 import * as xhr from 'xhr';
+import {user} from 'user';
 
 class LeftPanel extends React.Component<any, any> {
   
@@ -23,9 +24,11 @@ class LeftPanel extends React.Component<any, any> {
 class AccountBook extends React.Component<any, any> {
   constructor(props, context) {
     super(props, context);
+    this.state = {accountBooks: [], selectValue: ''};
+    this.requestAccountBookData('');
   }
   
-  requestAccountBookData(accountBook?: string) {
+  requestAccountBookData(accountBook: string) {
     let option: any = {
         handleAs: 'json',
         data: {
@@ -34,24 +37,35 @@ class AccountBook extends React.Component<any, any> {
     };
     xhr.post(`${Config.requestHost}/requestAccountBookData`, option)
     .then((data)=>{
-        if(!data.state || data.state != stateCode.SUCCESS || !data.user) {
-            topic.publish('tipService/warnning', data.info);
+        if(data.state === stateCode.SUCCESS) {
+          this.setState({accountBooks: data.accountBooks, selectValue: data.currentAccountBook});
+          topic.publish('financeapp/accountBookData', data);
+        } else if(data.state === stateCode.Error) {
+          topic.publish('tipService/warning', data.info);
+        } else if(data.state === stateCode.NOTLOGGIN) {
+          topic.publish('tipService/warning', data.info);
+          user.logout();
         } else {
-            topic.publish('financeapp/accountBookData', data.info);
+          topic.publish('tipService/warning', lang.xhrDataError);
         }
     }, (error)=>{
-        topic.publish('login/error', lang.xhrErr);
+        topic.publish('tipService/error', lang.xhrErr);
     })
+  }
+  
+  selectHandler(event) {
+    this.requestAccountBookData(event.target.value);
+    //this.setState({selectValue: event.target.value});
   }
   
   render() {
       return (
         <ul className="financeapp-panel-ul">
             <li className="financeapp-panel-items-active">{ lang.AccountBook }</li>
-            <select className="financeapp-panel-item-select">
-              <option>Mustard</option>
-              <option>Ketchup</option>
-              <option>Relish</option>
+            <select className="financeapp-panel-item-select" onChange={ this.selectHandler.bind(this) } value={ this.state.selectValue }>
+              {
+                this.state.accountBooks.map((accountBook)=><option key={ accountBook } value={ accountBook }>{ accountBook }</option>)
+              }
             </select>
         </ul>
       )
