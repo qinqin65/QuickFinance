ad386d95a89bf32c08662b846d85538945d0a440
qinqin65 <tom163com@126.com>
2016-07-31 21:26:55 +0800

    csrf改为实时获取

--- QuickFinance_frontEnd/app/config.ts
+++ QuickFinance_frontEnd/app/config.ts
@@ -1,7 +1,4 @@
-import * as cookie from 'dojo/cookie';
-
 export default class Config {
     static requestHost: string = 'http://localhost:8000/quick';
-    static csrf: string = cookie('csrftoken');
     static passwordMinLength: number = 6;
 }
