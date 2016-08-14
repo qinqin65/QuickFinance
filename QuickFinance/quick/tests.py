fb72d8e6d5b84584be8beb41850d93c4b8187cd7
tom <tom163com@126.com>
2016-08-14 21:13:23 +0800

    更新createuser的单元测试

--- QuickFinance/quick/tests.py
+++ QuickFinance/quick/tests.py
@@ -8,23 +8,17 @@ Replace this with more appropriate tests for your application.
 import time
 import django
 from django.test import TestCase
+
 from .util import createUserAndInit
 
 # TODO: Configure your database in settings.py and sync before running tests.
 
 class QuickTest(TestCase):
-    """Tests for the application views."""
-
-    if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
-        @classmethod
-        def setUpClass(cls):
-            django.setup()
 
     def createUserAndInitTest(self):
         timeStamp = int(time.time())
         userName = 'test' + str(timeStamp)
         password = '123456'
         email = None
-        createUserAndInit(userName, email, password)
-        # self.assertEqual(1 + 1, 2)
+        user = createUserAndInit(userName, email, password)
+        self.assertEqual(userName, user.username, 'the name of created user must match the userName which is auto genereted by time string')
