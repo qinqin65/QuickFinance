cb8aaabc7ad5de10709fe13f36b94cf833359594
tom <tom163com@126.com>
2016-08-14 20:12:31 +0800

    给部分表添加索引

--- QuickFinance/quick/models.py
+++ QuickFinance/quick/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class AccountBook(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True)
     accountBookName = models.CharField(max_length=200)
     remark = models.TextField()
 
@@ -13,7 +13,7 @@ class Currency(models.Model):
     name_en = models.CharField(max_length=20)
 
 class Account(models.Model):
-    accountBook = models.ForeignKey(AccountBook, on_delete=models.CASCADE)
+    accountBook = models.ForeignKey(AccountBook, on_delete=models.CASCADE, db_index=True)
     accountName = models.CharField(max_length=200)
     currency = models.ForeignKey(Currency, null=True, on_delete=models.SET_NULL)
     total = models.FloatField(default=0)
@@ -21,12 +21,12 @@ class Account(models.Model):
     remark = models.TextField()
 
 class AccountType(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, db_index=True)
     name = models.CharField(max_length=20)
     remark = models.TextField()
 
 class Income(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, db_index=True)
     type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
     date = models.DateTimeField()
     value = models.FloatField()
@@ -34,7 +34,7 @@ class Income(models.Model):
     remark = models.TextField()
 
 class Outcome(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, db_index=True)
     type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
     date = models.DateTimeField()
     value = models.FloatField()
@@ -42,7 +42,7 @@ class Outcome(models.Model):
     remark = models.TextField()
 
 class UserSetting(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, db_index=True)
     totalProperty = models.FloatField(default=0)
     defaultAccountBook = models.ForeignKey(AccountBook, null=True, on_delete=models.SET_NULL)
     defaultAccount = models.ForeignKey(Account, null=True, on_delete=models.SET_NULL)
