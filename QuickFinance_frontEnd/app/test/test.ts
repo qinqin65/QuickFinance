1dac12619b352c3a91919575290731143f68cc84
tom <tom163com@126.com>
2016-09-11 23:29:06 +0800

    完善后端getFinanceData及前端FinancePreviewStore的单元测试

--- QuickFinance_frontEnd/app/test/test.ts
+++ QuickFinance_frontEnd/app/test/test.ts
@@ -1,9 +1,13 @@
 import * as loginTest from 'loginTest';
+import * as utilTest from 'utilTest';
+import * as storeTest from 'storeTest';
 
 export function runTest() {
     mocha.setup('bdd');
     
     loginTest.describes();
+    utilTest.describes();
+    storeTest.describes();
     
     mocha.checkLeaks();
     mocha.run();
